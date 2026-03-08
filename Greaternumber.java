2ff2c59dbce869715b650df7805b7f051308b6ce
konasanibhavana <141711960+konasanibhavana@users.noreply.github.com>
2026-03-09 00:14:51 +0530

    Create Greaternumber.java

---
+++ Greaternumber.java
@@ -0,0 +1,13 @@
+public class Greaternumber{
+  static void main(String args[]) {
+	int num1=20;
+	int num2=40;
+	int max;
+	if(num1 > num2) {
+		System.out.println("Max=" + num1);
+	}
+	else {
+		System.out.println("Max="+ num2);
+	}
+}
+}
