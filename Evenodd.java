5106500bfb2762e36a516115651840e3f415128a
konasanibhavana <141711960+konasanibhavana@users.noreply.github.com>
2026-03-09 01:04:04 +0530

    Create Evenodd.java

---
+++ Evenodd.java
@@ -0,0 +1,12 @@
+public class Evenodd{
+ public static void main(String args[]){
+   Scanner sc=new Scanner(System.in);
+   int num=sc.nextInt();
+   if(num%i==0){
+    System.out.println("Even number");
+   }
+   else{
+    System.out.println("Odd number");
+   }
+}
+}
