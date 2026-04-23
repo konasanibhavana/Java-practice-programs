470687643bf8ec2985e4a6ff8752605508cff133
konasanibhavana <141711960+konasanibhavana@users.noreply.github.com>
2026-04-24 00:32:15 +0530

    Add Whileloopsum class to calculate sum of 1 to 100
    Implement a while loop to calculate the sum of integers from 1 to 100.

---
+++ whilesum.java
@@ -0,0 +1,13 @@
+public class Whileloopsum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int sum=0,i=1;
+     while(i<=100) {
+    	 sum=sum+i;
+    	 i++;		 
+     }
+     System.out.println("Result:"+sum);	
+	}
+
+}
