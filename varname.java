c4036573dbcb821181298308a093b176ff4f3c3d
konasanibhavana <141711960+konasanibhavana@users.noreply.github.com>
2026-04-24 00:30:05 +0530

    Add Varname class to check employee age

---
+++ varname.java
@@ -0,0 +1,13 @@
+public class Varname {
+   public void findoutvar() {
+	   int Employeeage=9;
+	   String Employeename="Bhavana";
+	   if(Employeeage>0 && Employeeage<10) {
+		   System.out.println("underage");	   
+		 }   
+   }
+	public static void main(String[] args) {
+	   Varname vn=new Varname();
+	   vn.findoutvar();
+   }
+}
