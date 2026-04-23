d272d901b646112a208c16105df6f36bb99a91f2
konasanibhavana <141711960+konasanibhavana@users.noreply.github.com>
2026-04-24 00:34:20 +0530

    Add functionality to find second largest number
    Implement logic to find the second largest number in an array.

---
+++ secondlargest.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Secondlargest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int [] arr= {1,5,3,0,12,10,11};//{0,1,3,5,10,11,12}
+     Arrays.sort(arr);	
+     for(int i=0;i<arr.length;i++) {  
+	}System.out.println(arr[arr.length-2]); 
+    	  }
+
+}
