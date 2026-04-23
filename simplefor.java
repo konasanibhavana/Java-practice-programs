bfa19ba4f564ce034dbc438355f172a36b883290
konasanibhavana <141711960+konasanibhavana@users.noreply.github.com>
2026-04-24 00:25:53 +0530

    Add Simpleforloop class to display numbers 1-10

---
+++ simplefor.java
@@ -0,0 +1,13 @@
+public class Simpleforloop {
+    public void display() {
+    	for(int i=1;i<=10;i++) {
+    		System.out.println(i);
+    	}
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Simpleforloop sfl=new Simpleforloop();
+      sfl.display();
+	}
+
+}
