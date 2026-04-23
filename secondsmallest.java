4573179fb7a10c5460ecd27924d1583a5b60b034
konasanibhavana <141711960+konasanibhavana@users.noreply.github.com>
2026-04-24 00:36:25 +0530

    Create secondsmallest.java

---
+++ secondsmallest.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Secondsmallest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int [] arr= {10,50,13,0,20,3,7};//{0,6,7,10,13,20,50}
+     Arrays.sort(arr);
+   	 System.out.println(arr[1]);
+    }
+ }
